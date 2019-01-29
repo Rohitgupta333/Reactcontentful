f63fac21a2174cf13fcdc26d113887107ac2837a
Rohitgupta333 <rohitguptab33@gmail.com>
2019-01-29 23:11:01 +0530

    added build files

---
+++ build/precache-manifest.b617ec04ea0d14d66cf53512822f619d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe78fb7e79d8fa5a372b",
+    "url": "/static/css/main.e85fb0c8.chunk.css"
+  },
+  {
+    "revision": "fe78fb7e79d8fa5a372b",
+    "url": "/static/js/main.fe78fb7e.chunk.js"
+  },
+  {
+    "revision": "79170b7dc6b99753c58c",
+    "url": "/static/css/1.2443ee08.chunk.css"
+  },
+  {
+    "revision": "79170b7dc6b99753c58c",
+    "url": "/static/js/1.79170b7d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bc46d6c04019f41081c4f530912035af",
+    "url": "/index.html"
+  }
+];
